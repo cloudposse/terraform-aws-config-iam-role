0aa3b0dcdb6ec0f40fb38acf20574453e87047d6
Matt Calhoun <matt@mattcalhoun.com>
2020-12-15 14:20:18 -0500

    update role creation

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ module "iam_role" {
   name         = "aws-config"
   use_fullname = true
 
-  policy_documents      = []
-  policy_document_count = 0
+  policy_document_count = 1
+  policy_documents      = [data.aws_iam_policy_document.config_s3_policy[0].json]
 
-  policy_description = "AWS Config IAM policy"
-  role_description   = "AWS Config IAM role"
+  policy_description = "AWS Config IAM Policy"
+  role_description   = "AWS Config IAM Role"
 
   context = module.this.context
 }
@@ -31,4 +31,29 @@ resource "aws_iam_role_policy_attachment" "config_policy_attachment" {
 
 data "aws_iam_policy" "aws_config_built_in_role" {
   arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
+}
+
+data "aws_iam_policy_document" "config_s3_policy" {
+  count = module.this.enabled ? 1 : 0
+
+  statement {
+    sid       = "AWSConfigBucketDelivery"
+    effect    = "Allow"
+    resources = [local.account_bucket_path]
+    actions   = ["s3:PutObject"]
+    condition {
+      test     = "StringLike"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Locals and Data References
+#-----------------------------------------------------------------------------------------------------------------------
+data "aws_caller_identity" "this" {}
+
+locals {
+  account_bucket_path = format("%s/AWSLogs/%s/Config/*", var.s3_bucket_arn, data.aws_caller_identity.this.account_id)
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "s3_bucket_arn" {
+  description = "The ARN of the AWS Config S3 bucket"
+  type        = string
+}
